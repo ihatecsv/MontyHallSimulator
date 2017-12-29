596324eb4ad46cac3d616afe96b7b17936c44c49
ihatecsv <me@drakeluce.com>
2017-12-29 17:32:42 -0400

    Added chalk logging

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const numberOfDoors = 3;
-const rounds = 100000;
+const chalk = require("chalk");
 
+const numberOfDoors = 3;
+const rounds = 1000;
+const delay = 50;
+
+let currentRound = 0;
 let switchWins = 0;
 
 const playRound = function(nod, sw){
@@ -27,12 +31,38 @@ const playRound = function(nod, sw){
 	return initialDoorChoice == randomCarDoor;
 }
 
-for(let i = 1; i <= rounds; i++){
-	if(playRound(numberOfDoors, true)){
-		switchWins++;
+const loopRound = function(){
+	process.stdout.write("\x1Bc");
+
+	const switchWon = playRound(numberOfDoors, true);
+
+	switchWins += switchWon ? 1 : 0;
+
+	let stayWins = currentRound - switchWins; //Rounds we have lost by switching are rounds that would have been won by staying
+
+	console.log(chalk.bgRed("Round result:"));
+	console.log(switchWon ? "Switching won!" : "Switching lost!");
+
+	console.log(chalk.bgGreen("Theoretical statistics:"));
+	console.log("Switch win: " + (((numberOfDoors-1)/numberOfDoors)*100).toFixed(1) + "%, Stay win: "
+		+ ((1/numberOfDoors)*100).toFixed(1) + "%, Number of doors: " + numberOfDoors);
+
+	console.log(chalk.bgCyan("Actual statistics:"));
+	console.log("Switch win: " + ((switchWins/currentRound)*100).toFixed(1) + "%, Stay win: "
+		+ ((stayWins/currentRound)*100).toFixed(1) + "%, Number of doors: " + numberOfDoors
+		+ ", Round: " + currentRound + "/" + rounds + ", Percentage complete: " + ((currentRound/rounds)*100).toFixed(0) + "%");
+
+	if(currentRound == rounds){
+		return;
 	}
 
-	let stayWins = i - switchWins; //Rounds we have lost by switching are rounds that would have been won by staying
-	console.log("Switch win: " + ((switchWins/i)*100).toFixed(1) + "%, Stay win: "
-		+ ((stayWins/i)*100).toFixed(1) + "%, Number of doors: " + numberOfDoors + ", Round: " + i + "/" + rounds);
+	currentRound++;
+
+	if(delay > 0){
+		setTimeout(loopRound, delay);
+	}else{
+		loopRound();
+	}
 }
+
+loopRound();
