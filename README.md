5b0f21a38c3b0705172dc21a6c1afbb3bf114d80
Drake Luce <me@drakeluce.com>
2018-07-18 09:44:27 -0300

    Added command line arguments, added example to readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # MontyHallSimulator
+
 Simulates the probabilities of the Monty Hall problem.
+
+```bash
+git clone https://github.com/ihatecsv/MontyHallSimulator.git
+cd MontyHallSimulator
+npm install
+node index.js [number_of_doors] [rounds] [round_delay]
+```
 
 ![Screenshot](https://raw.githubusercontent.com/ihatecsv/MontyHallSimulator/master/screenshot.png)
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const chalk = require("chalk");
 
-const numberOfDoors = 3;
-const rounds = 1000;
-const delay = 50;
+const numberOfDoors = process.argv[2] || 3;
+const rounds = process.argv[3] || 1000;
+const roundDelay = process.argv[4] || 50;
 
 let currentRound = 0;
 let switchWins = 0;
 
-const playRound = function(nod, sw){
-	const randomCarDoor = Math.floor(Math.random()*nod); //Choose a random door number to fill with a car
+const playRound = function(numberOfDoors, playerSwitched){
+	const randomCarDoor = Math.floor(Math.random()*numberOfDoors); //Choose a random door number to fill with a car
 
-	const initialDoorChoice = Math.floor(Math.random()*nod); //Contestant chooses a door
+	const initialDoorChoice = Math.floor(Math.random()*numberOfDoors); //Contestant chooses a door
 
 	let otherDoor = 0;
 	if(initialDoorChoice == randomCarDoor){
 		while(true){
-			otherDoor = Math.floor(Math.random()*nod); //Choose other door to keep closed from remaining doors
+			otherDoor = Math.floor(Math.random()*numberOfDoors); //Choose other door to keep closed from remaining doors
 			if(otherDoor != initialDoorChoice){
 				break;
 			}
@@ -24,7 +24,7 @@ const playRound = function(nod, sw){
 		otherDoor = randomCarDoor; //Choose other door to keep closed as the car door (only choice)
 	}
 
-	if(sw){
+	if(playerSwitched){
 		return otherDoor == randomCarDoor;
 	}
 
@@ -40,14 +40,14 @@ const loopRound = function(){
 
 	let stayWins = currentRound - switchWins; //Rounds we have lost by switching are rounds that would have been won by staying
 
-	console.log(chalk.bgRed("Round result:"));
+	console.log(chalk.red("Round result:"));
 	console.log(switchWon ? "Switching won!" : "Switching lost!");
 
-	console.log(chalk.bgGreen("Theoretical statistics:"));
+	console.log(chalk.green("Theoretical statistics:"));
 	console.log("Switch win: " + (((numberOfDoors-1)/numberOfDoors)*100).toFixed(1) + "%, Stay win: "
 		+ ((1/numberOfDoors)*100).toFixed(1) + "%, Number of doors: " + numberOfDoors);
 
-	console.log(chalk.bgCyan("Actual statistics:"));
+	console.log(chalk.cyan("Actual statistics:"));
 	console.log("Switch win: " + ((switchWins/currentRound)*100).toFixed(1) + "%, Stay win: "
 		+ ((stayWins/currentRound)*100).toFixed(1) + "%, Number of doors: " + numberOfDoors
 		+ ", Round: " + currentRound + "/" + rounds + ", Percentage complete: " + ((currentRound/rounds)*100).toFixed(0) + "%");
@@ -58,8 +58,8 @@ const loopRound = function(){
 
 	currentRound++;
 
-	if(delay > 0){
-		setTimeout(loopRound, delay);
+	if(roundDelay > 0){
+		setTimeout(loopRound, roundDelay);
 	}else{
 		loopRound();
 	}
